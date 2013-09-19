db7c3d0d58af3a291984750efeb2cd11ec9f66cb
Benjamin Bengfort <benjamin@bengfort.com>
2013-09-19 14:13:48 -0400

    slight improvements

--- spinnaker/downloader.py
+++ spinnaker/downloader.py
@@ -1,19 +1,30 @@
+import os
+import codecs
+import urlparse
+
+import w3lib.url as urls
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 
-
 class Downloader(object):
 
-    def __init__(self, driver="PhantomJS", timeout=30):
-        driver = getattr(webdriver, driver, None)
-        if not driver:
-            raise ImportError("Could not import selenium.webdriver.{0}.".format(driver))
-        self._browser = driver()
+    def __init__(self, htdocs, driver="PhantomJS", timeout=30):
+        self.htdocs = htdocs
+
+        self._driver  = driver
+        self._browser = self._create_browser(driver)
         self._browser.set_page_load_timeout(timeout)
         self._current_url = ""
 
     def __del__(self):
-        self._browser.close()
+        self._browser.quit()
+
+    def _create_browser(self, driver=None):
+        driver = driver or self._driver
+        driver = getattr(webdriver, driver, None)
+        if not driver:
+            raise ImportError("Could not import selenium.webdriver.{0}.".format(driver))
+        return driver()
 
     @property
     def annotated_source(self):
@@ -31,6 +42,7 @@ class Downloader(object):
         try:
             self._browser.get(url)
         except TimeoutException:
+            self._browser.quit()
             self._current_url = None
         self._current_url = self._browser.current_url
 
@@ -51,6 +63,45 @@ class Downloader(object):
     def url(self):
         return self._current_url
 
+    def filededup(self, path, count=0):
+        # This is the easy case- no duplication.
+        if not os.path.exists(path):
+            return path
+
+        # Set up work variables
+        count     += 1
+        dirp, name = os.path.split(path)
+        root, ext  = os.path.splitext(name)
+
+        # Ensure a number hasn't already been added to the name
+        if count > 1 and '-' in root and root[-1].isdigit():
+            root = '-'.join(root.split('-')[:-1])
+
+        name = "%s-%i%s" % (root, count, ext)
+
+        return self.filededup(os.path.join(dirp, name), count)
+
+    def write(self):
+        safe_url = urls.safe_download_url(self.url)
+        scheme, netloc, path, query, _ = urlparse.urlsplit(safe_url)
+        path = os.path.join(self.htdocs, netloc, path[1:])
+
+        if path.endswith('/'): path += "index.html"
+
+        # Replace this with mimetypes
+        if '.' not in os.path.basename(path):
+            path = os.path.join(path, "index.html")
+
+        pathdir = os.path.dirname(path)
+        if not os.path.exists(pathdir):
+            os.makedirs(pathdir)
+
+        # Increment duplication
+        path = self.filededup(path)
+
+        with codecs.open(path, 'w', encoding="utf-8") as out:
+            out.write(self.annotated_source)
+
 
 if __name__ == "__main__":
     url = (
@@ -58,6 +109,7 @@ if __name__ == "__main__":
         "offerid=256004.2419&type=15&murl=http%3A%2F%2Fwww."
         "7forallmankind.com%2Fpd%2Fp%2F2419.html"
     )
-    d = Downloader()
+    d = Downloader('../htdocs/')
     d.load(url)
+    d.write()
     d.source()
--- spinnaker/job.py
+++ spinnaker/job.py
@@ -1,28 +1,19 @@
-import codecs
-import os
-import re
 import sys
+import time
 import requests
-import w3lib.url as urls
-import urlparse
 import multiprocessing as mproc
 
 from downloader import Downloader
 from reader import Reader
 
-
-refresh = re.compile(r"^refresh$", re.I)
-content = re.compile(r"^(\d+);\s*url=(.+)$", re.I)
-
 class URLWorker(mproc.Process):
 
     def __init__(self, htdocs, tasks, results):
-        self.htdocs = htdocs
         self.tasks = tasks
         self.results = results
         super(URLWorker, self).__init__()
         sys.stdout.write('I')
-        self.downloader = Downloader()
+        self.downloader = Downloader(htdocs)
 
     def run(self):
         while True:
@@ -34,7 +25,6 @@ class URLWorker(mproc.Process):
                     break
                 status = self.fetch(task)
                 if status == 200:
-                    self.download()
                     sys.stdout.write('.')
                 elif status >= 300 and status <= 399:
                     sys.stdout.write('R')
@@ -50,20 +40,23 @@ class URLWorker(mproc.Process):
                 import traceback
                 print traceback.format_exc()
                 sys.stdout.write('E')
-                continue
+                break
 
             sys.stdout.flush()
+            time.sleep(1)
 
     def fetch(self, url):
         self.downloader.load(url)
         if not self.downloader.load_succeeded:
             return 0
+        else:
+            self.downloader.write()
         # It's necessary to do a second request to the URL to get a
-        # real status code. The reason is that it is impossible to 
+        # real status code. The reason is that it is impossible to
         # get a status code from selenium itself. This is a concious
         # decision on the part of the selenium devs. http://bit.ly/nlMvIM.
-        # So, it's necessary to check the status of the last URL in the 
-        # headless browser to make sure we are not saving a 3/4/500 page. 
+        # So, it's necessary to check the status of the last URL in the
+        # headless browser to make sure we are not saving a 3/4/500 page.
         headers = {'User-Agent': (
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) "
             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/2"
@@ -76,47 +69,6 @@ class URLWorker(mproc.Process):
             response = requests.get(self.downloader.url, headers=headers)
         return response.status_code
 
-        
-
-    def filededup(self, path, count=0):
-        # This is the easy case- no duplication.
-        if not os.path.exists(path):
-            return path
-
-        # Set up work variables
-        count     += 1
-        dirp, name = os.path.split(path)
-        root, ext  = os.path.splitext(name)
-
-        # Ensure a number hasn't already been added to the name
-        if count > 1 and '-' in root and root[-1].isdigit():
-            root = '-'.join(root.split('-')[:-1])
-
-        name = "%s-%i%s" % (root, count, ext)
-
-        return self.filededup(os.path.join(dirp, name), count)
-
-    def download(self):
-        safe_url = urls.safe_download_url(self.downloader.url)
-        scheme, netloc, path, query, _ = urlparse.urlsplit(safe_url)
-        path = os.path.join(self.htdocs, netloc, path[1:])
-
-        if path.endswith('/'): path += "index.html"
-
-        # Replace this with mimetypes
-        if '.' not in os.path.basename(path):
-            path = os.path.join(path, "index.html")
-
-        pathdir = os.path.dirname(path)
-        if not os.path.exists(pathdir):
-            os.makedirs(pathdir)
-
-        # Increment duplication
-        path = self.filededup(path)
-
-        with codecs.open(path, 'w', encoding="utf-8") as out:
-            out.write(self.downloader.annotated_source)
-
 
 class FetchJob(object):
 
@@ -127,7 +79,7 @@ class FetchJob(object):
         self.whitelist = Reader(whitelist)
         self.tasks     = mproc.Queue()
         self.results   = mproc.Queue()
-        self.numprocs  = kwargs.get('numprocs', mproc.cpu_count()) 
+        self.numprocs  = kwargs.get('numprocs', mproc.cpu_count())
         self._workers  = []
 
     @property
