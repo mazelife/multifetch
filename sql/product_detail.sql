7845c9b7ae01b73840f16e0e6064e22020b4194f
Benjamin Bengfort <benjamin@bengfort.com>
2013-10-16 17:49:14 -0400

    sql fix

--- sql/product_detail.sql
+++ sql/product_detail.sql
@@ -18,10 +18,10 @@
 BEGIN;
  
 COPY (SELECT product, url FROM (
-        SELECT vendor.name AS vendor, product.name AS product, COALESCE(shelf.detail_url, shelf.buy_url, NULL) AS url
-                from shelf
-		        join product on product.id = shelf.product_id
-		        join vendor on vendor.id = shelf.vendor_id) AS prods
+        SELECT merchant.name AS merchant, product.name AS product, COALESCE(sku.detail_url, sku.buy_url, NULL) AS url
+                from sku
+		        join product on product.id = sku.product_id
+		        join merchant on merchant.id = sku.merchant_id) AS prods
       WHERE url IS NOT NULL) 
     TO :'infile'
     WITH ENCODING 'utf8';
